e7d8c4e421fa1dc1f939e64da9e3352444630479
Arjun India <arjun8107@gmail.com>
2021-07-14 16:57:18 +0000

    lol

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header">
-      <Link to='/'>
+      <Link to="/">
         <div className="logo"></div>
       </Link>
       <div className="upload-container">
         <div className="section">
-          <Link to='/upload'>
+          <Link to="/upload">
             <div className="upload" />
           </Link>
-          <img className="personal" src="https://i.imgur.com/QwZod6m.png" />
+          <img
+            className="personal"
+            alt="Your_Image"
+            src="https://i.kym-cdn.com/photos/images/original/001/996/641/bc2.jpg"
+          />
         </div>
       </div>
     </div>
-  )  
-}
-  
-export default Header
+  );
+};
+
+export default Header;
